7ba7bee0163cc1599a51dc37cb4c6a91255345fd
JuankoGmzk <37718099+JuankoGmzk@users.noreply.github.com>
2021-05-20 15:30:13 -0500

    Se realiza validación en la creación de un usuario en la base de datos

--- Controller/profile.controller.js
+++ Controller/profile.controller.js
@@ -1,13 +1,26 @@
+const model  = require('../model/profile.model');
+const { ObjectId } = require('mongodb');
+
+
 require('../database/db.connection');
 
 const Profiles = {};
 
-Profiles.createProfile = async function(err, profile) {
-    console.log("Saving in DataBase -- > ",profile);
-    profile.save();
-    if (err) {
-       console.log(err); 
+Profiles.createProfile = async function (err, profile) {
+    
+    const mail = profile.mail;
+    const perfil = await model.findOne({mail})
+    if(!perfil){
+        console.log("Saving in DataBase -- > ", profile);
+        profile.save();
+        if (err) {
+            console.log(err);
+        }
     }
+    else{
+        console.log("Ya existe el usuario")
+    }
+
 }
 
 module.exports = Profiles;
