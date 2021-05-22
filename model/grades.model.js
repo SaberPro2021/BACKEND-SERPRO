cac5b66baa5e17144c7a05b83caa7c8eaa32e4ff
JuankoGmzk <37718099+JuankoGmzk@users.noreply.github.com>
2021-05-21 20:33:26 -0500

    Se crea el modelo grades

---
+++ model/grades.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const { ObjectId } = require('mongodb');
+const IcfesModule = require('../model/icfesModule.model');
+const IcfesTest = require('../model/icfesTest.model');
+const Profile = require('../model/profile.model');
+
+const {Schema} = mongoose;
+
+//SCHEME OF THE MODULE COLLECTION
+const gradesSchema = new Schema ({
+
+    userId :{type:ObjectId, ref: Profile},
+    moduleId: {type:ObjectId, ref: IcfesModule},
+    testId: {type:ObjectId, ref:IcfesTest},
+    time: Number,
+    score: String
+},{autoCreate: true});
+
+
+module.exports = mongoose.model('grades',gradesSchema);
