21f96ac6bdd1c0fdb04084233b0332ebabccee5b
WS <volfram@gmail.com>
2024-08-15 00:42:18 -0500

    improve

--- database/db.connection.js
+++ database/db.connection.js
@@ -5,10 +5,10 @@ const devUrl = `mongodb+srv://${dbconfig.USER}:${dbconfig.PASSWORD}@${dbconfig.H
 const prodUrl = `mongodb+srv://${process.env.MONGODB_USR}:${process.env.MONGODB_PWD}@${process.env.MONGODB_HOST}/${process.env.MONGODB_DBNAME}`;
 const uri = process.env.APP_ENVIRONMENT == 'prod' ? prodUrl : devUrl;
 
-const uriRedis = {URL : 'redis://127.0.0.1:6379' };
+const uriRedis = `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`;
 const db = mongoose.connection;
 
-console.log('WE ARE CONNECTED TO', uri);
+console.log('***DB=' + uri + '\n***REDIS=' + uriRedis);
 
 //CONFIRMATION OF THE CONNECTION WITH THE DATABASE
 db.on('error', console.error.bind(console, 'connection error:'));
